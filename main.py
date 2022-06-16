c08258153fae950ee45d2e68b11bc855467c04cf
ViMuilu <vmuilu@gmail.com>
2022-06-16 11:36:28 +0300

    added user info processing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 import json
 import secrets
 import logging
+from tokenize import String
 
 from urllib.parse import urlencode
 from distutils.util import strtobool
@@ -12,11 +13,20 @@ from typing import Tuple
 
 import httpx
 
+import aiohttp
+from typing import Optional
+
 from fastapi import FastAPI, Cookie
 from fastapi.exceptions import HTTPException
 from fastapi.responses import PlainTextResponse, RedirectResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+from authlib.jose import jwt
+from authlib.jose import JWTClaims
+
+import base64
+import ujson
+from typing import Dict, List, Tuple
 # the web app
 app = FastAPI()
 
@@ -282,7 +292,6 @@ async def token_endpoint(access_token: str = Cookie("")):
     headers = {
         "Authorization": f"Bearer {access_token}",
     }
-
     async with httpx.AsyncClient(headers=headers, verify=False) as client:
         # request tokens
         LOG.debug("requesting userinfo")
@@ -291,8 +300,165 @@ async def token_endpoint(access_token: str = Cookie("")):
             # return userinfo content
             LOG.debug("received userinfo")
             r = response.json()
+            r["Dataset access"] = []
+            passportList = []
+            for passportVisas in r["ga4gh_passport_v1"]:
+                decoded = await decode_passport(passportVisas)
+               
+                if decoded != "":
+                    r["Dataset access"].append(decoded)
+                    r["Dataset access"].append("teststst")
+                    r["Dataset access"].append("teasassda")
+                
+                if decoded == "AcceptedTermsAndPoliciesTrue":
+                    passportList.append(decoded)
+                if decoded == "ResearcherStatusTrue":
+                    passportList.append(decoded)
+            if await get_ga4gh_bona_fide(passportList):
+                r["Bona fide status"] = "Verified"
+            else:
+                r["Bona fide status"] = "Unverified"  
+            r.pop("given_name", None)
+            r.pop("family_name", None)
+            r.pop("family_name", None)
+            r.pop("ga4gh_passport_v1", None)
+            r["email"] = r["email"], r["email_verified"]
+            r.pop("email_verified", None)
+
             return r
         else:
             # if something went wrong on the provider side, we need to abort
             LOG.error(f"didn't receive userinfo from OpenID provider: {response.status_code}")
             raise HTTPException(500, f"failed to retrieve userinfo from provider: {response.status_code}")
+
+
+async def decode_passport(encoded_passport: str) -> List[Dict]:
+    """Return decoded header and payload from encoded passport JWT.
+    Public-key-less decoding inspired by the PyJWT library https://github.com/jpadilla/pyjwt
+    This function is originally from https://github.com/CSCfi/beacon-python with some changes made to suit the needs of this project.
+    """
+    LOG.debug("Decoding GA4GH passport.")
+
+    # Convert the token string into bytes for processing, and split it into segments
+    decoded_passport = encoded_passport.encode("utf-8")  # `header.payload.signature`
+    data, _ = decoded_passport.rsplit(b".", 1)  # data contains header and payload segments, the ignored segment is the signature segment
+    segments = data.split(b".", 1)  # [header, payload]
+
+    # Intermediary container
+    verified_segments = []
+    # Handle different implementations of public exponent
+    # 65537 is recommended, but some use 65536 instead
+    # pad the missing bytes, if needed
+    for segment in segments:
+        rem = len(segment) % 4
+        if rem > 0:
+            segment += b"=" * (4 - rem)
+        verified_segments.append(segment)
+
+    # Decode the verified token segments
+    decoded_segments = [base64.urlsafe_b64decode(seg) for seg in verified_segments]
+
+    # Convert the decoded segment bytes into dicts for easy access
+    decoded_data = [ujson.loads(seg.decode("utf-8")) for seg in decoded_segments]
+    if decoded_data[1]["ga4gh_visa_v1"]["type"] == "ControlledAccessGrants":
+        return decoded_data[1]["ga4gh_visa_v1"]["value"]
+    elif decoded_data[1]["ga4gh_visa_v1"]["type"]  == "AcceptedTermsAndPolicies":
+        return "AcceptedTermsAndPoliciesTrue"
+    elif decoded_data[1]["ga4gh_visa_v1"]["type"]  == "ResearcherStatusTrue":
+        return "ResearcherStatusTrue"
+    else:
+        return ""    
+        
+
+async def get_ga4gh_bona_fide(passports: List) -> bool:
+    """Retrieve Bona Fide status from GA4GH JWT claim.
+    This function is originally from https://github.com/CSCfi/beacon-python"""
+    LOG.info("Parsing GA4GH bona fide claims.")
+
+    # User must have agreed to terms, and been recognized by a peer to be granted Bona Fide status
+    terms = False
+    status = False
+
+    for passport in passports:
+        # Check for the `type` of visa to determine if to look for `terms` or `status`
+        #
+        # CHECK FOR TERMS
+        passport_type = passport[2].get("ga4gh_visa_v1", {}).get("type")
+        passport_value = passport[2].get("ga4gh_visa_v1", {}).get("value")
+        if passport_type in "AcceptedTermsAndPolicies" and passport_value == "https://doi.org/10.1038/s41431-018-0219-y":
+            # This passport has the correct type and value, next step is to validate it
+            #
+            # Decode passport and validate its contents
+            # If the validation passes, terms will be set to True
+            # If the validation fails, an exception will be raised
+            # (and ignored since it's not fatal), and terms will remain False
+            await validate_passport(passport)
+            # The token is validated, therefore the terms are accepted
+            terms = True
+        #
+        # CHECK FOR STATUS
+        if passport_value == "https://doi.org/10.1038/s41431-018-0219-y" and passport_type == "ResearcherStatus":
+            # Check if the visa contains a bona fide value
+            # This passport has the correct type and value, next step is to validate it
+            #
+            # Decode passport and validate its contents
+            # If the validation passes, status will be set to True
+            # If the validation fails, an exception will be raised
+            # (and ignored since it's not fatal), and status will remain False
+            await validate_passport(passport)
+            # The token is validated, therefore the status is accepted
+            status = True
+
+        # User has agreed to terms and has been recognized by a peer, return True for Bona Fide status
+    return terms and status
+
+async def validate_passport(passport: Dict) -> JWTClaims:
+    """Decode a passport and validate its contents.
+    This function is originally from https://github.com/CSCfi/beacon-python"""
+    LOG.debug("Validating passport.")
+
+    # Passports from `get_ga4gh_controlled()` will be of form
+    # passport[0] -> encoded passport (JWT)
+    # passport[1] -> unverified decoded header (contains `jku`)
+    # Passports from `get_bona_fide_status()` will be of form
+    # passport[0] -> encoded passport (JWT)
+    # passport[1] -> unverified decoded header (contains `jku`)
+    # passport[2] -> unverified decoded payload
+
+    # JWT decoding and validation settings
+    # The `aud` claim will be ignored, because Beacon has no prior knowledge
+    # as to where the token has originated from, and is therefore unable to
+    # verify the intended audience. Other claims will be validated as per usual.
+    claims_options = {"aud": {"essential": False}}
+
+    # Attempt to decode the token and validate its contents
+    # None of the exceptions are fatal, and will not raise an exception
+    # Because even if the validation of one passport fails, the query
+    # Should still continue in case other passports are valid
+    try:
+        # Get JWK for this passport from a third party provider
+        # The JWK will be requested from a URL that is given in the `jku` claim in the header
+        passport_key = await get_jwk(passport[1].get("jku"))
+        # Decode the JWT using public key
+        decoded_passport = jwt.decode(passport[0], passport_key, claims_options=claims_options)
+        # Validate the JWT signature
+        decoded_passport.validate()
+        # Return decoded and validated payload contents
+        return decoded_passport
+    except Exception as e:
+        LOG.error(f"Something went wrong when processing JWT tokens: {e}")
+
+async def get_jwk(url: str) -> Optional[Dict]:
+    """Get JWK set keys to validate JWT.
+    This function is originally from https://github.com/CSCfi/beacon-python"""
+    LOG.debug("Retrieving JWK.")
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as r:
+                # This can be a single key or a list of JWK
+                return await r.json()
+    except Exception:
+        # This is not a fatal error, it just means that we are unable to validate the permissions,
+        # but the process should continue even if the validation of one token fails
+        LOG.error(f"Could not retrieve JWK from {url}")
+        return None
